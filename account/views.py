a48813802e88a0251321197ec651304aebf9d141
sinsohi_04 <sohi8451@naver.com>
2023-10-05 17:18:16 +0900

    account 추가

--- account/views.py
+++ account/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import forms, login,authenticate
 
 # Create your views here.
+def show_register(request):
+    if request.method == "POST":
+        form = forms.UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(False)
+            user.save()
+            return render(request, "account/complete.html",{"username":user.username})
+    else:
+        form = forms.UserCreationForm()
+        
+    return render(request, "account/register.html",{"form":form})
+
+def show_login(request):
+    if request.method == "POST":
+        form = forms.AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username = username, password = password)
+            if user is not None:
+                login(request,user)
+                return redirect("url:index")
+            
+    else:
+        form = forms.AuthenticationForm()
+        
+    return render(request, "account/register.html",{"form":form})
+
+        
